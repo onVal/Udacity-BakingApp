9bf1c4b63100e7950004afeafdd7cf9c81bfa76d
onval <giorgio.valbonesi@gmail.com>
2017-09-25 01:31:51 +0200

    Add NetworkUtilities class

---
+++ app/src/main/java/com/onval/bakingapp/NetworkUtilities.java
@@ -0,0 +1,12 @@
+package com.onval.bakingapp;
+
+/**
+ * Created by gval on 25/09/2017.
+ */
+
+public class NetworkUtilities {
+    public static boolean isOnline() {
+        //todo: to implement
+        return false;
+    }
+}
