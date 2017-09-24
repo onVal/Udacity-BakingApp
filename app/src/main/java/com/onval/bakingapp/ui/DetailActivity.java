9ec447cbb6be8193a90f8a67275cf8ca8d213a00
onval <giorgio.valbonesi@gmail.com>
2017-09-24 23:45:34 +0200

    Refactor packages

--- app/src/main/java/com/onval/bakingapp/ui/DetailActivity.java
+++ app/src/main/java/com/onval/bakingapp/ui/DetailActivity.java
@@ -1,7 +1,7 @@
 package com.onval.bakingapp.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.onval.bakingapp.R;
 
--- app/src/main/java/com/onval/bakingapp/ui/RecipeFragment.java
+++ app/src/main/java/com/onval/bakingapp/ui/RecipeFragment.java
@@ -8,9 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.onval.bakingapp.R;
+import com.onval.bakingapp.Recipe;
+import com.onval.bakingapp.RecipePresenter;
+
+import java.util.Set;
 
 
-public class RecipeFragment extends Fragment {
+public class RecipeFragment extends Fragment implements RecipePresenter.View {
 
     public RecipeFragment() {
         // Required empty public constructor
@@ -20,5 +24,10 @@ public class RecipeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_recipe, container, false);
+    }
+
+    @Override
+    public void onAddRecipes(Set<Recipe> recipes) {
+
     }
 }
