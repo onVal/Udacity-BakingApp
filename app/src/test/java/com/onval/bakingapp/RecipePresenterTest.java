e680a28ce908f714a6e990318cc62726e2f8977d
onval <giorgio.valbonesi@gmail.com>
2017-09-27 15:02:57 +0200

    Update two previous tests

--- app/src/test/java/com/onval/bakingapp/RecipePresenterTest.java
+++ app/src/test/java/com/onval/bakingapp/RecipePresenterTest.java
@@ -1,7 +1,16 @@
 package com.onval.bakingapp;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import junit.framework.Assert;
+
+import org.json.JSONArray;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by gval on 25/09/2017.
@@ -20,12 +29,81 @@ public class RecipePresenterTest {
 
     @Test
     public void shouldPassRecipesToView() {
+        view = new MockView();
+        model = new MockFetcher();
+
+        presenter = new RecipePresenter(view, model);
+
+        presenter.loadRecipes();
+
+        Assert.assertEquals(4, ((MockView) view).numOfRecipes);
 
     }
 
     @Test
     public void shouldHandleNoRecipes() {
+        view = new MockView();
+        model = new NoFetcher();
 
+        presenter = new RecipePresenter(view, model);
+
+        presenter.loadRecipes();
+
+        Assert.assertTrue(((MockView) view).displayErrorMsgCalled);
+    }
+
+    //Mock classes
+    private class MockView implements IView {
+        int numOfRecipes;
+        boolean displayErrorMsgCalled;
+
+        @Override
+        public void onAddRecipes(Set<Recipe> recipes) {
+            numOfRecipes = recipes.size();
+        }
+
+        @Override
+        public void displayErrorMsg(String msg) {
+            displayErrorMsgCalled = true;
+        }
+
+        @Override
+        public void onNoInternetConnection() {
+
+        }
+    }
+
+    private class MockFetcher implements IFetcher {
+        @Override
+        public void fetchFromServer(Response.Listener<JSONArray> responseListener,
+                                    Response.ErrorListener errorListener) {
+            responseListener.onResponse(new JSONArray());
+        }
+
+        @Override
+        public HashSet<Recipe> parseRecipes(JSONArray json) {
+            HashSet<Recipe> recipes = new HashSet<>();
+
+            recipes.add(new Recipe("cake", "", 3));
+            recipes.add(new Recipe("meat", "", 5));
+            recipes.add(new Recipe("fish", "", 7));
+            recipes.add(new Recipe("rice", "", 9));
+
+            return recipes;
+        }
+    }
+
+    private class NoFetcher implements IFetcher {
+        @Override
+        public void fetchFromServer(Response.Listener<JSONArray> responseListener,
+                                    Response.ErrorListener errorListener) {
+            errorListener.onErrorResponse(new VolleyError());
+        }
+
+        @Override
+        public HashSet<Recipe> parseRecipes(JSONArray json) {
+            return null;
+        }
     }
 
 }
